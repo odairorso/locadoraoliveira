fdd35b6268ac9e7d858d93852340203f106e5dc5
Odair3341 <140565467+Odair3341@users.noreply.github.com>
2025-08-07 15:45:00 -0400

    fix: Remove anonymous sign-in

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -2,13 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { supabase } from './supabase.ts'
-
-supabase.auth.getSession().then(({ data: { session } }) => {
-  if (!session) {
-    supabase.auth.signInAnonymously()
-  }
-})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
